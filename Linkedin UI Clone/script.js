abd366de594c0d139ddd2763e711b5bcec474f5c
chaanakyaaM <chaanakyaam@gmail.com>
2024-08-17 18:33:24 +0530

    icon bottom bar animated

--- Linkedin UI Clone/script.js
+++ Linkedin UI Clone/script.js
@@ -4,18 +4,19 @@ const jobs = document.querySelector('#nav-jobs')
 const msg = document.querySelector("#nav-messaging")
 const noti = document.querySelector('#nav-notification')
 const down = document.querySelector('.down')
-network.addEventListener('click',()=>{
-    down.style.left='-438px';
-});
+down.style.left='-635px';
 home.addEventListener('click',()=>{
-    down.style.left='-525px';
+    down.style.left='-635px';
+});
+network.addEventListener('click',()=>{
+    down.style.left='-528px';
 });
 jobs.addEventListener('click',()=>{
-    down.style.left='-350px';
+    down.style.left='-420px';
 });
 msg.addEventListener('click',()=>{
-    down.style.left='-270px';
+    down.style.left='-325px';
 });
 noti.addEventListener('click',()=>{
-    down.style.left='-175px';
+    down.style.left='-210px';
 });
