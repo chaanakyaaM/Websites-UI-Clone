2f96e78076efe009799ec15ddd467e652c850735
chaanakyaaM <chaanakyaam@gmail.com>
2024-08-18 18:21:58 +0530

    hover effects and transition durations are added

--- Linkedin UI Clone/script.js
+++ Linkedin UI Clone/script.js
@@ -1,28 +1,30 @@
-const network = document.querySelector('#nav-network'),
-    home = document.querySelector('#nav-home'),
-    jobs = document.querySelector('#nav-jobs'),
-    msg = document.querySelector("#nav-messaging"),
-    noti = document.querySelector('#nav-notification'),
-    down = document.querySelector('.down'),
-    like = document.querySelector('.like')
-down.style.left='-635px';
-home.addEventListener('click',()=>{
-    down.style.left='-635px';
+const network = document.querySelector("#nav-network"),
+  home = document.querySelector("#nav-home"),
+  jobs = document.querySelector("#nav-jobs"),
+  msg = document.querySelector("#nav-messaging"),
+  noti = document.querySelector("#nav-notification"),
+  down = document.querySelector(".down"),
+  like = document.querySelector(".like");
+down.style.left = "-635px";
+home.addEventListener("click", () => {
+  down.style.left = "-635px";
 });
-network.addEventListener('click',()=>{
-    down.style.left='-528px';
+network.addEventListener("click", () => {
+  down.style.left = "-528px";
 });
-jobs.addEventListener('click',()=>{
-    down.style.left='-420px';
+jobs.addEventListener("click", () => {
+  down.style.left = "-420px";
 });
-msg.addEventListener('click',()=>{
-    down.style.left='-325px';
+msg.addEventListener("click", () => {
+  down.style.left = "-325px";
 });
-noti.addEventListener('click',()=>{
-    down.style.left='-210px';
+noti.addEventListener("click", () => {
+  down.style.left = "-210px";
 });
-const hov = document.querySelector('.hov')
-like.addEventListener('mouseover',()=>{
-     hov.style.animation='name 3s ease-in-out;'
-})
-
+const hov = document.querySelector(".hov");
+like.addEventListener("mouseover", () => {
+  hov.style.animation = "name 3s ease-in-out";
+});
+like.addEventListener("mouseleave", () => {
+  hov.style.animation = "none";
+});
